0692b57eb78e141fbfa8ebc932740992b32b7d14
Quinn Stroube <q.stroube@gmail.com>
2022-06-11 14:59:25 -0500

    Add files via upload
    Part 1-3 working

--- Challenge Files/loan_analyzer.py
+++ Challenge Files/loan_analyzer.py
@@ -127,23 +127,30 @@ else:                                                                           
 # """
 
 # # Given the following loan data, you will need to calculate the present value for the loan
-# new_loan = {
-#     "loan_price": 800,
-#     "remaining_months": 12,
-#     "repayment_interval": "bullet",
-#     "future_value": 1000,
-# }
+new_loan = {
+    "loan_price": 800,
+    "remaining_months": 12,
+    "repayment_interval": "bullet",
+    "future_value": 1000,
+}
 
 # # @TODO: Define a new function that will be used to calculate present value.
 # #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 # #    The function should return the `present_value` for the loan.
 # # YOUR CODE HERE!
+new_loan["annual_discount_rate"] = .2
+def calculate_present_value(future_value,remaining_months,annual_discount_rate):
+    present_value = future_value / (1 + annual_discount_rate/12) ** remaining_months
+    print("PV",present_value)
+    return present_value
 
+pv_loan = calculate_present_value(new_loan["future_value"],new_loan["remaining_months"],new_loan["annual_discount_rate"])
+print(f"The present value of the loan is: {pv_loan: .2f}")
 
 # # @TODO: Use the function to calculate the present value of the new loan given below.
 # #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # # YOUR CODE HERE!
-# print(f"The present value of the loan is: {present_value}")
+
 
 
 # """Part 4: Conditionally filter lists of loans.
